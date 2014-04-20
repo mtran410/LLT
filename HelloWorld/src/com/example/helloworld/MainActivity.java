d2e6459e1daac6c3137ff541d8a69f959671c3d1
Preston Lomenzo <prestonlomenzo@hotmail.com>
2014-04-19 20:56:34 -0700

    Formatted a bit

--- HelloWorld/src/com/example/helloworld/MainActivity.java
+++ HelloWorld/src/com/example/helloworld/MainActivity.java
@@ -13,42 +13,35 @@ import android.view.Menu;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
-
 	TextView textLIGHT_available, textLIGHT_reading;
-	 Uri notification;
-	 Ringtone r;
+	Uri notification;
+	Ringtone r;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		 textLIGHT_available 
-         = (TextView)findViewById(R.id.LIGHT_available);
-        textLIGHT_reading 
-         = (TextView)findViewById(R.id.LIGHT_reading);
+		textLIGHT_available = (TextView)findViewById(R.id.LIGHT_available);
+        textLIGHT_reading = (TextView)findViewById(R.id.LIGHT_reading);
         
         SensorManager mySensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
         
         Sensor LightSensor = mySensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
         if(LightSensor != null){
-         textLIGHT_available.setText("Sensor.TYPE_LIGHT Available");
-         mySensorManager.registerListener(
-           LightSensorListener, 
-           LightSensor, 
-           SensorManager.SENSOR_DELAY_NORMAL);
-         
-        }else{
-         textLIGHT_available.setText("Sensor.TYPE_LIGHT NOT Available");
-        }
+        	textLIGHT_available.setText("Sensor.TYPE_LIGHT Available");
+        	mySensorManager.registerListener(
+            LightSensorListener, 
+            LightSensor, 
+            SensorManager.SENSOR_DELAY_NORMAL);
+        } else
+        	textLIGHT_available.setText("Sensor.TYPE_LIGHT NOT Available");
         
         try {
 			 notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
 			 r = RingtoneManager.getRingtone(getApplicationContext(), notification);
-		  
-		 } catch (Exception e) {
+		} catch (Exception e) {
 		    e.printStackTrace();
-		 }
-       
-
+		}
 	}
 
 	@Override
@@ -58,27 +51,18 @@ public class MainActivity extends Activity {
 		return true;
 	}
 	
-	private final SensorEventListener LightSensorListener
-	     = new SensorEventListener(){
-
-		
+	private final SensorEventListener LightSensorListener = new SensorEventListener(){
 	   @Override
 	   public void onAccuracyChanged(Sensor sensor, int accuracy) {
 	    // TODO Auto-generated method stub
-	    
 	   }
 
 	   @Override
 	   public void onSensorChanged(SensorEvent event) {
-		 if(event.sensor.getType() == Sensor.TYPE_LIGHT){
+		 if(event.sensor.getType() == Sensor.TYPE_LIGHT)
 		     textLIGHT_reading.setText("LIGHT: " + event.values[0]);
-		    }
-		 if(event.values[0]>200){
+		 if(event.values[0]>200)
 			 r.play();
-		 }
-		
 	   }
-	   
 	};
-
 }
