1448c2edf2c0aa9994e228c2b7a68a8e91e5466a
Preston Lomenzo <prestonlomenzo@hotmail.com>
2014-04-24 12:08:28 -0700

    Uploaded default avatars

--- LocalLaserTag COMBINED/gen/llc/locallasertag/R.java
+++ LocalLaserTag COMBINED/gen/llc/locallasertag/R.java
@@ -22,7 +22,42 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int black_overlay=0x7f040000;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int a1=0x7f020000;
+        public static final int a10=0x7f020001;
+        public static final int a11=0x7f020002;
+        public static final int a12=0x7f020003;
+        public static final int a13=0x7f020004;
+        public static final int a14=0x7f020005;
+        public static final int a15=0x7f020006;
+        public static final int a16=0x7f020007;
+        public static final int a17=0x7f020008;
+        public static final int a18=0x7f020009;
+        public static final int a19=0x7f02000a;
+        public static final int a2=0x7f02000b;
+        public static final int a20=0x7f02000c;
+        public static final int a21=0x7f02000d;
+        public static final int a22=0x7f02000e;
+        public static final int a23=0x7f02000f;
+        public static final int a24=0x7f020010;
+        public static final int a25=0x7f020011;
+        public static final int a26=0x7f020012;
+        public static final int a27=0x7f020013;
+        public static final int a28=0x7f020014;
+        public static final int a29=0x7f020015;
+        public static final int a3=0x7f020016;
+        public static final int a30=0x7f020017;
+        public static final int a31=0x7f020018;
+        public static final int a32=0x7f020019;
+        public static final int a33=0x7f02001a;
+        public static final int a34=0x7f02001b;
+        public static final int a35=0x7f02001c;
+        public static final int a4=0x7f02001d;
+        public static final int a5=0x7f02001e;
+        public static final int a6=0x7f02001f;
+        public static final int a7=0x7f020020;
+        public static final int a8=0x7f020021;
+        public static final int a9=0x7f020022;
+        public static final int ic_launcher=0x7f020023;
     }
     public static final class id {
         public static final int container=0x7f070001;
