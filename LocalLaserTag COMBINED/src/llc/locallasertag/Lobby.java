1cb9c23b8e6ab99abd4e11f37acd68eca360d71d
Preston Lomenzo <prestonlomenzo@hotmail.com>
2014-05-01 20:29:28 -0700

    Tried to integrate Sangs code, updated to have MyPlayer and Player class

--- LocalLaserTag COMBINED/src/llc/locallasertag/Lobby.java
+++ LocalLaserTag COMBINED/src/llc/locallasertag/Lobby.java
@@ -1,8 +1,10 @@
 package llc.locallasertag;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 
 public class Lobby extends Activity {
 
@@ -22,4 +24,10 @@ public class Lobby extends Activity {
       }
       return super.onOptionsItemSelected(item);
    }
+
+   public void joinGame(View v) {
+      Intent intent = new Intent(this, TimerActivity.class);
+      startActivity(intent);
+   }
+
 }
