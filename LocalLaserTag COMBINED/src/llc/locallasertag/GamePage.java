10b742bc0a45c977dd27a90b04ee1724e94a7739
Jonathan <jonquybao@yahoo.com>
2014-05-11 13:37:12 -0700

    revert final

--- LocalLaserTag COMBINED/src/llc/locallasertag/GamePage.java
+++ LocalLaserTag COMBINED/src/llc/locallasertag/GamePage.java
@@ -1,12 +1,14 @@
 package llc.locallasertag;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 
 public class GamePage extends Activity{
-	private Button conGame1, conGame2, conGame3, conGame4;
+	private Button conGame1, conGame2, conGame3, conGame4, createGame;
 	private int numGame, numplayerGame1, numplayerGame2, numplayerGame3, numplayerGame4;
 	private int totalPlayerGame1, totalPlayerGame2, totalPlayerGame3, totalPlayerGame4;
 	private String game1Name, game2Name, game3Name, game4Name;
@@ -18,6 +20,14 @@ public class GamePage extends Activity{
 		  conGame2 =(Button)findViewById(R.id.game2);
 		  conGame3 =(Button)findViewById(R.id.game3);
 		  conGame4 =(Button)findViewById(R.id.game4);
+		  createGame = (Button)findViewById(R.id.butCreateGame);
+		  
+		  createGame.setOnClickListener(new OnClickListener() {
+	    	  public void onClick(View v){
+	    		  startActivity(new Intent(GamePage.this, CreateGame.class));
+	    	  }
+	      });
+		  
 		  
 		  
 	}
