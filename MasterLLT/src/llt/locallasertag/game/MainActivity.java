25fe4aa17fd980097751f46fd8ae58fcc82a9874
Preston Lomenzo <prestonlomenzo@hotmail.com>
2014-05-21 22:03:29 -0700

    settings are finished
    merge settings.java and app_settings.xml, will not work until server is
    edited first...

--- MasterLLT/src/llt/locallasertag/game/MainActivity.java
+++ MasterLLT/src/llt/locallasertag/game/MainActivity.java
@@ -30,155 +30,143 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
-	Button startButton, how;
-	private JSONObject jObject;
-	private String name;
-	private Activity mainActivity;
+   Button startButton, how;
+   private JSONObject jObject;
+   private String name;
+   private Activity mainActivity;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		mainActivity=this;
-		
-      Typeface start = Typeface.createFromAsset(getAssets(),
-            "fonts/moonhouse.ttf");
-    TextView tv = (TextView) findViewById(R.id.startbutton);
-    tv.setTypeface(start);
-    
-    Typeface htp = Typeface.createFromAsset(getAssets(),
-          "fonts/moonhouse.ttf");
-  TextView tx = (TextView) findViewById(R.id.howtoplay);
-  tx.setTypeface(htp);
+   @Override
+   protected void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      setContentView(R.layout.activity_main);
+      mainActivity = this;
 
-  Typeface setting = Typeface.createFromAsset(getAssets(),
-        "fonts/moonhouse.ttf");
-TextView ts = (TextView) findViewById(R.id.setting);
-ts.setTypeface(setting);
+      Typeface start = Typeface.createFromAsset(getAssets(), "fonts/moonhouse.ttf");
+      TextView tv = (TextView) findViewById(R.id.startbutton);
+      tv.setTypeface(start);
 
-		
+      Typeface htp = Typeface.createFromAsset(getAssets(), "fonts/moonhouse.ttf");
+      TextView tx = (TextView) findViewById(R.id.howtoplay);
+      tx.setTypeface(htp);
+
+      Typeface setting = Typeface.createFromAsset(getAssets(), "fonts/moonhouse.ttf");
+      TextView ts = (TextView) findViewById(R.id.setting);
+      ts.setTypeface(setting);
+
    }
-   
- 
 
-	@Override
-	protected void onStart() {
-		// TODO Auto-generated method stub
-		super.onStart();
-		name = getUsername();
-		Log.d("NAME", name);
-		// Player myplayer = new Player(name);
-		new LongOperation().execute("");
+   @Override
+   protected void onStart() {
+      // TODO Auto-generated method stub
+      super.onStart();
+      name = getUsername();
+      Log.d("NAME", name);
+      // Player myplayer = new Player(name);
+      new LongOperation().execute("");
 
-	}
+   }
 
-	// TALK TO SERVER START
-	private class LongOperation extends AsyncTask<String, Void, String> {
+   // TALK TO SERVER START
+   private class LongOperation extends AsyncTask<String, Void, String> {
 
-		@Override
-		protected String doInBackground(String... params) {
+      @Override
+      protected String doInBackground(String... params) {
 
-			String temp;
+         String temp;
 
-			temp = "http://www.jonquybao.com/LLT/feedurls/v_login.php?username="
-					+ name;
-			// +Integer.parseInt((playerHealth.get(0)*100)+"")
-			Log.d("LOOK", temp);
-			jObject = JSONfunctions.getJSONfromURL(temp);
-			return "Executed";
-		}
+         temp = "http://www.jonquybao.com/LLT/feedurls/v_login.php?username=" + name;
+         // +Integer.parseInt((playerHealth.get(0)*100)+"")
+         Log.d("LOOK", temp);
+         jObject = JSONfunctions.getJSONfromURL(temp);
+         return "Executed";
+      }
 
-		@Override
-		protected void onPostExecute(String result) {
+      @Override
+      protected void onPostExecute(String result) {
 
-			Log.d("JSON", jObject.toString());
-			try {
+         Log.d("JSON", jObject.toString());
+         try {
 
-				JSONArray jArray;
+            JSONArray jArray;
 
-				jArray = jObject.getJSONArray("player");
+            jArray = jObject.getJSONArray("player");
 
-				for (int i = 0; i < jArray.length(); i++) {
-					JSONObject object = jArray.getJSONObject(i);
+            for (int i = 0; i < jArray.length(); i++) {
+               JSONObject object = jArray.getJSONObject(i);
 
+               if (object.has("pid")) {
+                  SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(mainActivity);
+                  Editor edit = settings.edit();
+                  edit.putInt("pid", Integer.parseInt(object.getString("pid")));
+                  edit.putString("team", object.getString("team"));
+                  edit.apply();
 
-					if (object.has("pid")) {
-						SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(mainActivity);
-						Editor edit = settings.edit();
-						edit.putInt("pid", Integer.parseInt(object.getString("pid")));
-						edit.putString("team", object.getString("team"));
-						edit.apply(); 
+               }
 
+               //Log.d("PID", Integer.parseInt(object.getString("team"))+"");
 
-					}
-				
-					//Log.d("PID", Integer.parseInt(object.getString("team"))+"");
-				
-				
-				}
-			} catch (JSONException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-	
-		}
+            }
+         } catch (JSONException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+         }
 
-		@Override
-		protected void onPreExecute() {
-		}
+      }
 
-		@Override
-		protected void onProgressUpdate(Void... values) {
-		}
-	}
+      @Override
+      protected void onPreExecute() {
+      }
 
-	// TALK TO SERVER END
-	
-	
-	public String getUsername() {
-		AccountManager manager = AccountManager.get(this);
-		Account[] accounts = manager.getAccountsByType("com.google");
-		List<String> possibleEmails = new LinkedList<String>();
+      @Override
+      protected void onProgressUpdate(Void... values) {
+      }
+   }
 
-		for (Account account : accounts) {
-			// TODO: Check possibleEmail against an email regex or treat
-			// account.name as an email address only for certain account.type
-			// values.
-			possibleEmails.add(account.name);
-		}
+   // TALK TO SERVER END
 
-		if (!possibleEmails.isEmpty() && possibleEmails.get(0) != null) {
-			String email = possibleEmails.get(0);
-			String[] parts = email.split("@");
-			if (parts.length > 0 && parts[0] != null)
-				return parts[0];
-			else
-				return null;
-		} else
-			return null;
-	}
+   public String getUsername() {
+      AccountManager manager = AccountManager.get(this);
+      Account[] accounts = manager.getAccountsByType("com.google");
+      List<String> possibleEmails = new LinkedList<String>();
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
+      for (Account account : accounts) {
+         // TODO: Check possibleEmail against an email regex or treat
+         // account.name as an email address only for certain account.type
+         // values.
+         possibleEmails.add(account.name);
+      }
 
-		// Inflate the menu; this adds items to the action bar if it is present.
-		// TODO getMenuInflater().inflate(R.menu.main, menu);
+      if (!possibleEmails.isEmpty() && possibleEmails.get(0) != null) {
+         String email = possibleEmails.get(0);
+         String[] parts = email.split("@");
+         if (parts.length > 0 && parts[0] != null)
+            return parts[0];
+         else
+            return null;
+      } else
+         return null;
+   }
 
-		return true;
-	}
+   @Override
+   public boolean onCreateOptionsMenu(Menu menu) {
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		// TODO if (id == R.id.action_settings) {
-		// TODO return true;
-		// TODO }
-		return super.onOptionsItemSelected(item);
-	}
+      // Inflate the menu; this adds items to the action bar if it is present.
+      // TODO getMenuInflater().inflate(R.menu.main, menu);
 
+      return true;
+   }
+
+   @Override
+   public boolean onOptionsItemSelected(MenuItem item) {
+      // Handle action bar item clicks here. The action bar will
+      // automatically handle clicks on the Home/Up button, so long
+      // as you specify a parent activity in AndroidManifest.xml.
+      int id = item.getItemId();
+      // TODO if (id == R.id.action_settings) {
+      // TODO return true;
+      // TODO }
+      return super.onOptionsItemSelected(item);
+   }
 
    public void gameStart(View v) {
       Intent intent = new Intent(this, Lobby.class);
