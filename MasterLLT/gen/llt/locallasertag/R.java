647610187af35fae85e5954290f131023725f4c5
Preston Lomenzo <prestonlomenzo@hotmail.com>
2014-05-31 12:22:39 -0700

    most aesthetics

--- MasterLLT/gen/llt/locallasertag/R.java
+++ MasterLLT/gen/llt/locallasertag/R.java
@@ -72,20 +72,21 @@ public final class R {
         public static final int ic_launcher=0x7f020033;
         public static final int ic_launcher_llt=0x7f020034;
         public static final int lazerlight=0x7f020035;
-        public static final int logo1=0x7f020036;
-        public static final int logo3=0x7f020037;
-        public static final int logo4=0x7f020038;
-        public static final int logo6=0x7f020039;
-        public static final int mainpic=0x7f02003a;
-        public static final int pic1=0x7f02003b;
-        public static final int pic2=0x7f02003c;
-        public static final int pic3=0x7f02003d;
-        public static final int pic4=0x7f02003e;
-        public static final int pic5=0x7f02003f;
-        public static final int pic8=0x7f020040;
-        public static final int redprogressbar=0x7f020041;
-        public static final int settings=0x7f020042;
-        public static final int yellowprogressbar=0x7f020043;
+        public static final int lobbylist=0x7f020036;
+        public static final int logo1=0x7f020037;
+        public static final int logo3=0x7f020038;
+        public static final int logo4=0x7f020039;
+        public static final int logo6=0x7f02003a;
+        public static final int mainpic=0x7f02003b;
+        public static final int pic1=0x7f02003c;
+        public static final int pic2=0x7f02003d;
+        public static final int pic3=0x7f02003e;
+        public static final int pic4=0x7f02003f;
+        public static final int pic5=0x7f020040;
+        public static final int pic8=0x7f020041;
+        public static final int redprogressbar=0x7f020042;
+        public static final int settings=0x7f020043;
+        public static final int yellowprogressbar=0x7f020044;
     }
     public static final class id {
         public static final int Death=0x7f090024;
